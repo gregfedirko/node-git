4e5f334749ec1aa4e30fc790de3a5e27303e431b
Greg Fedirko <gjfedirko@gmail.com>
2015-01-19 20:04:26 -0800

    refactor test file structure

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
 gulp.task('test', function() {
-  return gulp.src('./spec/*.js')
+  return gulp.src('./spec/**/*.js')
     .pipe(mocha({reporter: 'nyan'}));
 })
 
