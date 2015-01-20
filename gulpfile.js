98d9590da0d1eead2e788e794ec194f936dd2eb9
Greg Fedirko <gjfedirko@gmail.com>
2015-01-19 23:06:46 -0800

    gulp watch-test

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,12 @@ var mocha = require('gulp-mocha');
 gulp.task('test', function() {
   return gulp.src('./spec/**/*.js')
     .pipe(mocha({reporter: 'nyan'}));
-})
+});
 
-gulp.task('default', ['test']);
+gulp.task('watch-test', function() {
+  gulp.watch('./components/**/*.js', ['test']);
+});
+
+
+
+gulp.task('default', ['watch-test']);
